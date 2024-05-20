ba0563af4344dde44627cc67540d8fb9d026ab18
Abdullah30nov <abdullahbinaslam64@gmail.com>
2024-05-20 21:10:52 +0500

    Web into App

---
+++ app.js
@@ -0,0 +1,100 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyD78lvgHoNNXXfi2yo5Kpd5i1UrNaTB5do",
+    authDomain: "todo-app-test-71431.firebaseapp.com",
+    databaseURL: "https://todo-app-test-71431-default-rtdb.firebaseio.com",
+    projectId: "todo-app-test-71431",
+    storageBucket: "todo-app-test-71431.appspot.com",
+    messagingSenderId: "679826278245",
+    appId: "1:679826278245:web:4e9d282e86c6ffc636438c"
+};
+
+var app = firebase.initializeApp(firebaseConfig);
+var db = firebase.database()
+
+firebase.database().ref("Todos").on("child_added", function (data) {
+    // Get the value of the input field
+    var input = document.getElementById('inputField').value;
+    
+    // If input field is empty, show an alert
+    if (input === "") {
+        alert('Please fill this field');
+    } else {
+        // Create a new list item
+        var liElement = document.createElement('li');
+        var liText = document.createTextNode(data.val().value);
+        liElement.appendChild(liText);
+
+        // Create edit button
+        var editbtn = document.createElement('button');
+        editbtn.textContent = "Edit";
+        editbtn.setAttribute("class", "libtn");
+        editbtn.setAttribute("onclick", "editLibtn(this)");
+        editbtn.setAttribute("id", data.key);
+        liElement.appendChild(editbtn);
+
+        // Create delete button
+        var delbtn = document.createElement('button');
+        delbtn.textContent = 'Delete';
+        delbtn.setAttribute("class", "libtn");
+        delbtn.setAttribute("onclick", "delLibtn(this)");
+        delbtn.setAttribute("id", data.key);
+        liElement.appendChild(delbtn);
+
+        // Append list item to the list
+        document.getElementById('list').appendChild(liElement);
+    }
+});
+
+// Function to add a new todo
+function addtodo() {
+    var input = document.getElementById('inputField').value;
+    var key = Date.now().toString(25);
+
+    // Check if input field is empty
+    if (input === "") {
+        alert("Please enter a todo.");
+    } else {
+        // Save todo to the database
+        firebase.database().ref("Todos/" + key).set({
+            value: input,
+            key: key
+        });
+        // Clear input field
+        document.getElementById('inputField').value = "";
+    }
+}
+
+// Function to delete a todo
+function delLibtn(e) {
+    var key = e.id;
+    // Remove todo from the database
+    firebase.database().ref("Todos/" + key).remove();
+    // Remove todo from the DOM
+    e.parentNode.remove();
+}
+
+// Function to edit a todo
+function editLibtn(e) {
+    var val = e.parentNode.firstChild.nodeValue;
+    var editvalue = prompt("Enter update value", val);
+
+    // Check if editvalue is empty or null
+    if (editvalue === "" || editvalue === null) {
+        alert("Please enter a value.");
+    } else {
+        // Update todo in the database
+        firebase.database().ref("Todos/" + e.id).update({
+            value: editvalue
+        });
+        // Update todo in the DOM
+        e.parentNode.firstChild.nodeValue = editvalue;
+    }
+}
+
+// Function to delete all todos
+function deleteAll() {
+    // Remove all todos from the database
+    firebase.database().ref("Todos").remove();
+    // Clear the list in the DOM
+    document.getElementById('list').innerHTML = "";
+}
